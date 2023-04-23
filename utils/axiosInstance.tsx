09148432b49bf6b9c70e18977178eb7b93ee2639
ido.Adar <idox2x@gmail.com>
2023-04-24 01:08:08 +0300

    socket io init implementation done!

--- utils/axiosInstance.tsx
+++ utils/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Config from 'react-native-config';
 
-// const development = Config.localhost;
+const development = Config.localhost;
 const production = Config.production;
 
 const axiosInstance = axios.create({
--- utils/socketIO.tsx
+++ utils/socketIO.tsx
@@ -1,9 +1,5 @@
 import {createContext} from 'react';
 import SocketIO from 'socket.io-client';
-import Config from 'react-native-config';
 
-// const development = Config.localhost;
-const production = Config.production;
-
-export const socket = SocketIO(production!);
+export const socket = SocketIO('https://wavesounds.onrender.com/');
 export const SocketContext = createContext({});
