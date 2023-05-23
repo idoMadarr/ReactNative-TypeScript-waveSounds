1658d811e8c632af22693a4bdcf26a507d574e07
ido.Adar <idox2x@gmail.com>
2023-05-24 00:19:43 +0300

    configure kotlin version

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import 'react-native-gesture-handler';
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 
 AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
